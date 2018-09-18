cdb1dbe56b211aeb7ce5e03ab713c0e85d6e5864
jardelufc <jardel@ufc.br>
2018-09-18 16:18:11 -0300

    qsort and cmake

--- awidRTDDOS/prob1.c
+++ awidRTDDOS/prob1.c
@@ -1,14 +1,17 @@
 #include"defs.h"
 float prob1(char * sourceIPs, char *sourceIP, int len){
     int i,j,count;
-    count = 0;
-    for (i=0;i<(len-1);i++){
-       if (!strcmp(sourceIPs + 18*i,sourceIP)){
+    count = 1;
+    i=1;
+    //for (i=0;i<(len-1);i++){
+       // como o vetor estar ordenado, ele só vai ser percorrido enquanto não chegar no próximo ip
+       while (!strcmp(sourceIPs + 18*i,sourceIP)) {
     // conta
         count = count + 1;
+        i++;
        }
 
-    }
+    //}
     return (float)count/(float)(len-1);
 
 }
--- awidRTDDOS/sourceIPsEntropy.c
+++ awidRTDDOS/sourceIPsEntropy.c
@@ -1,15 +1,48 @@
 #include"defs.h"
+#include <stdlib.h>
+#include <string.h>
+
+       static int
+       cmpstringp(const void *p1, const void *p2)
+       {
+           /* The actual arguments to this function are "pointers to
+              pointers to char", but strcmp(3) arguments are "pointers
+              to char", hence the following cast plus dereference */
+
+           return strcmp(* (char * const *) p1, * (char * const *) p2);
+       }
+
+
 float sourceIPsEntropy(char *sourceIPs,int len){
 //    size_t len = strlen(sourceIPs);
     int i,j;
     float result = 0.0;
+    
+    float total=0.0;
 
-    for (i=0;i<(len-1);i++) {
+    // ordena a lista de IPs
+    qsort(sourceIPs, len - 1, 18, strcmp);
+    
+    // calcula a probabilidade do primeiro ip da lista ordenada
+    result=prob1(sourceIPs, sourceIPs,len);
 
-      result=prob1(sourceIPs, sourceIPs+18*i,len);
-      printf("prob de %s é %f\n",sourceIPs+18*i,result);
+    printf("prob de %s é %f\n",sourceIPs,result);
+    // acumula ...e no final o acumulado tem q dar 1
+    total+=result;
+
+    // varre o restante da lista ordenada, por isso i comeca em 1
+    for (i=1;i<(len-1);i++) {
+
+      // calcula e acumula a probabilidade do elemento i, caso já não tenha sido calculada, por isso comparação 
+      if (strcmp(sourceIPs+18*(i-1),sourceIPs+18*i)) {
+		result=prob1(sourceIPs+18*i, sourceIPs+18*i,len);
+		total+=result;
+      		printf("prob de %s é %f\n",sourceIPs+18*i,result);
+      }
 
     }
+
+    return total;
 
     /*for (i=0;i<(len-1);i++){
        printf("sourceIPs=%s\n", sourceIPs+18*i);
@@ -26,7 +59,7 @@ float sourceIPsEntropy(char *sourceIPs,int len){
         }
     }*/
 
-    return result;
+    //return result;
 
 
 }
