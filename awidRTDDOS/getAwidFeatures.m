5dab2438b320e5207cd9849a77eb30233960a92c
Bruno Riccelli <riccellisp456@gmail.com>
2018-10-19 00:23:28 -0300

    some changes: TODO : modularize the code to perform each module on a function

--- awidRTDDOS/getAwidFeatures.m
+++ awidRTDDOS/getAwidFeatures.m
@@ -13,99 +13,118 @@ for file=1:length(files)
     interval = 0:((length(dataset)/155)-1);
     sourceIPs = dataset(79 +155*[interval]);
     destinationIPs = dataset(77 +155*[interval]);
-%  Removing invalid IPs
+    %  Removing invalid IPs
     sourceIPs = sourceIPs(~strcmp(destinationIPs,'?'));
     destinationIPs = destinationIPs(~strcmp(destinationIPs,'?'));
-
+    
     idx = find(~strcmp(destinationIPs,'?'));
-
-%  Filtering time
+    
+    %  Filtering time
     time = str2double(dataset(4 +155*[interval]));
     time = time(idx);
-%   Filtering responses    
+    %   Filtering responses
     response = dataset(155 +155*[interval]);
     response = response(idx);
-    keyboard;
+    %     keyboard;
     
-    firstPacketWindowTime = time(1) + 1;
-    n = 0;
-    nextPkt = 1;
-    janelas = 0;
-    numeroAtaques = 0;
-    numeroNormal = 0;
-    flagAttack = 0;
-    flagNormal = 0;
-    falsePositive = 0;
-    falseNegative = 0;
-    errors = 0;
+%     n = 0;
+%     nextPkt = 1;
+%     janelas = 0;
+%     numeroAtaques = 0;
+%     numeroNormal = 0;
+%     flagAttack = 0;
+%     flagNormal = 0;
+%     falsePositive = 0;
+%     falseNegative = 0;
+%     errors = 0;
     
-%     todo
-    for k = 1:length(unique(destinationIPs))
-    idxDestination = find(strcmp(destinationIPs, destinationIPs(k)));
-    end
+    %     todo
+    uniqueDestinations =  unique(destinationIPs);
     
-    while(nextPkt < length(idx))
-%       Splitting in seconds        
-        for i = nextPkt:length(idx)
-            if(~isWindow(time(nextPkt),time(i),windowLength))
-                n = n + 1;
+    for k = 1:length(uniqueDestinations)
+        idxDestination = find(strcmp(destinationIPs, uniqueDestinations(k)));
+        
+        sourceByDestination = (sourceIPs(idxDestination));
+        timeByDestination = time(idxDestination);
+        responseByDestination = response(idxDestination);
+        keyboard;
+% % % % % %         
+        n = 0;
+        nextPkt = 1;
+        janelas = 0;
+        numeroAtaques = 0;
+        numeroNormal = 0;
+        flagAttack = 0;
+        flagNormal = 0;
+        falsePositive = 0;
+        falseNegative = 0;
+        errors = 0;
+        
+        
+        
+        while(nextPkt < length(idx))
+            %       Splitting in seconds
+            for i = nextPkt:length(idx)
+                if(~isWindow(timeByDestination(nextPkt),timeByDestination(i),windowLength))
+                    n = n + 1;
+                else
+                    break;
+                end
+            end
+            %       Geting true responses
+            if (~isempty(find(strcmp(response(nextPkt:n),'normal') ~= 1 )))
+                disp('tem ataque real');
+                flagAttack = 1;
             else
-                break;
+                flagNormal = 1;
             end
-        end
-%       Geting true responses        
-        if (~isempty(find(strcmp(response(nextPkt:n),'normal') ~= 1 )))
-            disp('tem ataque real');
-            flagAttack = 1;
-        else
-            flagNormal = 1;
-        end
-        
-%         Getting Features
-        
-        entropySourceIPs= entropy2([sourceIPs(nextPkt:n)]);
-        varSourceIPs = length(unique([sourceIPs(nextPkt:n)]))/length(sourceIPs);
-        packetRate = n;
-%         Detection Module
-        NaHidModule = NaHid([packetRate varSourceIPs entropySourceIPs],normalTraffic);
-        janelas = janelas + 1;
-        nextPkt = n + 1;
-        
-        if(NaHidModule <  threshold)
-            disp('traffic');
-            disp(janelas);
-            disp('atack');
-            numeroAtaques = numeroAtaques + 1;
             
-%                 Getting false negatives and errors
+            %         Getting Features
             
-            if(flagNormal)
-                falsePositive= falsePositive + 1;
-                errors = errors + 1;
+            entropySourceIPs= entropy2([sourceIPs(nextPkt:n)]);
+            varSourceIPs = length(unique([sourceIPs(nextPkt:n)]))/length(sourceIPs);
+            packetRate = n;
+            %         Detection Module
+            NaHidModule = NaHid([packetRate varSourceIPs entropySourceIPs],normalTraffic);
+            janelas = janelas + 1;
+            nextPkt = n + 1;
+            
+            if(NaHidModule <  threshold)
+                disp('traffic');
+                disp(janelas);
+                disp('atack');
+                numeroAtaques = numeroAtaques + 1;
+                
+                %                 Getting false negatives and errors
+                
+                if(flagNormal)
+                    falsePositive= falsePositive + 1;
+                    errors = errors + 1;
+                end
+                %             plotResultsAwid(NaHidModule,threshold,entropySourceIPs,varSourceIPs,packetRate);
+                
+                
+            else
+                disp('traffic');
+                disp(janelas);
+                disp('normal');
+                
+                normalTraffic = [packetRate varSourceIPs entropySourceIPs];
+                numeroNormal = numeroNormal+ 1;
+                
+                if(flagAttack)
+                    %                 Getting false negatives and errors
+                    falseNegative = falseNegative + 1;
+                    errors = errors + 1;
+                end
+                %             plotResultsAwid(NaHidModule,threshold,entropySourceIPs,varSourceIPs,packetRate);
+                
             end
-%             plotResultsAwid(NaHidModule,threshold,entropySourceIPs,varSourceIPs,packetRate);
-
             
-        else
-            disp('traffic');
-            disp(janelas);
-            disp('normal');
+            %       Getting detection rate
+            accuracy = (1 - errors/janelas);
             
-            normalTraffic = [packetRate varSourceIPs entropySourceIPs];
-            numeroNormal = numeroNormal+ 1;
-            
-            if(flagAttack)
-%                 Getting false negatives and errors
-                falseNegative = falseNegative + 1;
-                errors = errors + 1;
-            end
-%             plotResultsAwid(NaHidModule,threshold,entropySourceIPs,varSourceIPs,packetRate);
-
         end
-        
-%       Getting detection rate        
-        accuracy = (1 - errors/janelas);
-        
     end
     cd testesplit
 end
